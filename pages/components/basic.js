6b2960c109f5bc5f55d74c824796a0c8714851ff
Fate 468 <115021956+Nidal468@users.noreply.github.com>
2023-02-17 10:03:14 +0000

    Initial commit

---
+++ pages/components/basic.js
@@ -0,0 +1,90 @@
+import {nav, card, load, home} from '/styles/style';
+import {motion} from 'framer-motion';
+
+import skills from '/data/skill.json';
+export function Nav(){
+	return(
+		<nav className={nav.body}>
+			<motion.div initial={{ 
+				x: -200,
+					}}
+					whileInView={{
+						x: 0,
+					}}
+					transition={{duration: 1}}
+				viewport={{once:true}}
+				>
+				<span>
+				<i className="fi fi-brands-youtube"></i>
+				<i className="fi fi-brands-discord"></i>
+				<i className="fi fi-brands-linkedin"></i>
+				<i className="fi fi-brands-facebook"></i>
+			</span>
+			</motion.div>
+			<motion.div initial={{ 
+				y: -50,
+					}}
+					whileInView={{
+						y: 0,
+					}}
+					transition={{duration: 1}}
+				viewport={{once:true}}
+				>
+			<div className={nav.contact}>
+				<i className="fi fi-rr-envelope"></i><h6>Contact me</h6>
+			</div>
+			</motion.div>
+		</nav>
+	)
+}
+export function Card(){
+	return(
+		<div className={card.body}>
+			<img/>
+			<div className={card.info}>
+				<span>
+				<h2>Classic junior dev portfolio</h2>
+				</span>
+				<p>This is a classic junior developer portfolio that I made with next js in my free time.</p>
+				<span>
+					<small>made by</small>
+					<small>Next JS</small>
+				</span>
+				</div>
+			</div>
+	)
+}
+export function Loading(props){
+	return(
+		<div className={load.body} style={{display: props.display}}>
+			<i className="fi fi-rr-spinner" id={load.spiner}></i>
+			<h2>Please wait patiently</h2>
+		</div>
+	)
+}
+export function Skill(){
+	return(
+		<motion.div initial={{
+			x: -100,
+			opacity: 0, 
+					}}
+					whileInView={{
+			x: 0, 
+			opacity: 1,
+					}}
+					transition={{duration: 1}}
+					viewport={{once:true}}
+					>
+					
+				<div className={home.skill}>
+					{skills.map(skill =>{
+						return(
+							<span key={skill.id}>
+								<i  id={home.icon} className={skill.icon}></i>
+								<h4 id={home.skill}>{skill.percent}</h4></span>
+						)
+					})}
+				</div>
+					</motion.div>
+	)
+}
---
+++ styles/style.js
@@ -0,0 +1,6 @@
+import home from './Home.module.css';
+import nav from './Nav.module.css';
+import card from './card.module.css';
+import load from './loading.module.css';
+
+export {home, nav, card, load};
